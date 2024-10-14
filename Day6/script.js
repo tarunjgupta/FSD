a84e3d5534c3bad066c91eb2c93915f02132dc80
tarunjgupta <tarunjgupta01136@gmail.com>
2024-10-14 13:06:56 +0530

    changess

---
+++ Day6/script.js
@@ -0,0 +1,54 @@
+ //CREATING FUNCTIONS IN JAVA SCRIPT
+// let a=()=>{
+//     console.log("Arrow Function");
+// };
+// let b=()=>console.log("Arrow Function 2");
+// b();
+// a();
+
+// let c=(x,y)=>
+// {
+//     return x+" "+y;
+// }
+// let res=c('Rahul',"Ravi");
+// let res1=c(12,32);
+// console.log(res);
+// console.log(res1);
+
+
+// 1
+// function display()
+// {
+//     //let p1=document.querySelector('p');
+//     let p1=document.querySelectorAll("p");/// selects all p tag by index
+//     p1[0].innerHTML="I am in para";
+//     p1[0].style.backgroundColor="red";
+//     p1[1].innerHTML="I am in second para";
+//     p1[1].style.backgroundColor="green";
+//     p1[2].innerHTML="I am in third para";
+//     p1[2].style.backgroundColor="pink";
+// }
+
+// let changeheader=()=>
+// {
+// let head1=document.querySelector("h1");
+// head1.textContent="Change the title";//changing heading content.
+// let a1=document.querySelector('a');
+// a1.setAttribute("href","https://aktu.ac.in");
+// a1.innerHTML="AKTU WEBSITE";
+// };
+
+
+
+
+
+// 2
+// let addcontent=()=>
+// {
+//     const newDiv=document.createElement('div');
+//     newDiv.textContent='This is a new div';
+//     const body=document.querySelector("body");
+//     body.appendChild(newDiv);
+// }
+
+
