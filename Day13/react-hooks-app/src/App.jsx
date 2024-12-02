bfcaab93bb0cacafa217cb1cfbd55ff8242a7ace
tarunjgupta <tarunjgupta01136@gmail.com>
2024-12-02 13:08:20 +0530

    day13

---
+++ Day13/react-hooks-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+/*import CounterApp from './components/CounterApp'*/
+import ImageApp from './components/ImageApp';
+const App = () => {
+  return (
+    <> 
+    {/*<CounterApp/>*/}
+      <ImageApp/>
+    </>
+  );
+};
+
+export default App
